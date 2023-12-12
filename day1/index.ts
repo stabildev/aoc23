94b3efd35d90d8842c63f7ec0794d9e9fb98d312
Henrik Stabil <dev@hardcoded.digital>
2023-12-12 20:25:40 +0100

    Add comments to day 1

--- day1/index.ts
+++ day1/index.ts
@@ -1,15 +1,6 @@
 import * as fs from 'fs'
 
 const digitMap: Record<string, number> = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
   '1': 1,
   '2': 2,
   '3': 3,
@@ -19,10 +10,21 @@ const digitMap: Record<string, number> = {
   '7': 7,
   '8': 8,
   '9': 9,
+  // Comment out the following lines for the part 1 solution
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
 }
 
 const keys = Object.keys(digitMap)
 
+// Not the most efficient algorithm but gets the job done
 const findDigits = (input: string) => {
   let first: number, last: number
   outer: for (let i = 0; i <= input.length; i++) {
@@ -49,8 +51,10 @@ const input = fs.readFileSync(`${__dirname}/input.txt`).toString().trim()
 
 const rows = input.split('\n')
 
-const numbers = rows.map((row) => findDigits(row))
+const numbers = rows.map((row) => findDigits(row)) // e.g. [[5, 4], ...]
 
+// Concatenate both digits as strings and parse the result as number
+// E.g. [5, 4] => 54
 const result = numbers.reduce((a, b) => a + Number(`${b[0]}${b[1]}`), 0)
 
 console.log(result)
