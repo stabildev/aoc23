4bf76a0b7c908785e6c06143c5026d7445e05eff
Henrik Stabil <dev@hardcoded.digital>
2023-12-14 03:13:41 +0100

    day 4 pt. 2

--- day4/index.ts
+++ day4/index.ts
@@ -11,10 +11,43 @@ const cards = input
     yours: yours.split(/\s+/),
   }))
 
-const points = cards
-  .map(({ winning, yours }) => yours.filter((x) => winning.includes(x)).length)
+const matches = cards.map(
+  ({ winning, yours }) => yours.filter((x) => winning.includes(x)).length
+)
+
+const points = matches
   .filter(Boolean) // remove zero points
   .map((matches) => 2 ** (matches - 1))
-  .reduce((a, b) => a + b, 0) // sum
 
-console.log(points)
+const totalPoints = points.reduce((a, b) => a + b, 0) // sum
+
+console.log(totalPoints)
+
+// Part 2
+
+// index in original cards array <-> quantity
+// default 1 for every index at beginning
+const numbersOfCards = new Map(cards.map((_, i) => [i, 1]))
+
+// loop over all cards
+for (let i = 0; i < cards.length; i++) {
+  // how many copies do we already have of this card?
+  // initially one plus all the already won ones
+  const copies = numbersOfCards.get(i)!
+
+  // for each copy we win one additional copy of the n following
+  // cards, where n is the number of matches of the current card
+  for (let j = i + 1; j <= i + matches[i]; j++) {
+    // update the number of cards to increase by the number of copies
+    // of the current card
+    numbersOfCards.set(j, numbersOfCards.get(j)! + copies)
+  }
+}
+
+// sum up all card counts
+const totalNumberOfCards = Array.from(numbersOfCards.values()).reduce(
+  (a, b) => a + b,
+  0
+)
+
+console.log(totalNumberOfCards)
